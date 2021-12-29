86f40973e8810f730094795267d3f1f5dacbf120
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2021-12-30 00:23:22 +0800

    initialize: homepage

---
+++ swiper.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  speed: 600,
+
+  // size
+  // width: 100,
+
+  // auto-play
+  // autoplay: {
+  // delay: 5000,
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
